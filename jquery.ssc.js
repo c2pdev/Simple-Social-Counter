43125aee9964f1eac63a31995bd7ed5c0bb46066
Dawid Myszczyszyn <dawidmyszczyszyn@gmail.com>
2013-07-21 13:13:06 +0100

    Lekcja 2

--- jquery.ssc.js
+++ jquery.ssc.js
@@ -19,13 +19,22 @@
             {
                 if (social[i] == 'twitter') //poczatek instrukcji warunkowej if/else
                 {
-                    $(target).append('<img src="' + options.twitter_ico + '"/>' + social[i] + '<br>'); //wyswietlamy gdy twitter
+                    $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=' + options.url + '&callback=?', function (data)
+                    {
+                        $(target).append('<span class="ssc_main_twitter"><span class="ssc_ico_twitter"><img src="' + options.twitter_ico + '"/></span><span class="ssc_count_twitter">' + (data.count || 0) + '</span></span>'); //wyswietlamy gdy twitter
+                    });
                 } else if (social[i] == 'facebook')
                 {
-                    $(target).append('<img src="' + options.facebook_ico + '"/>' + social[i] + '<br>'); //wyswietlamy gdy facebook
+                    $.getJSON('http://graph.facebook.com/' + options.url + '?callback=?', function (data)
+                    {
+                        $(target).append('<span class="ssc_main_facebook"><span class="ssc_ico_facebook"><img src="' + options.facebook_ico + '"/></span><span class="ssc_count_facebook">' + (data.shares || 0) + '</span></span>'); //wyswietlamy gdy facebook
+                    });
                 } else if (social[i] == 'linkedin')
                 {
-                    $(target).append('<img src="' + options.linkedin_ico + '"/>' + social[i] + '<br>'); //wyswietlamy gdy linkedin
+                    $.getJSON('http://www.linkedin.com/countserv/count/share?url=' + options.url + '&lang=en_US&callback=?', function (data)
+                    {
+                        $(target).append('<span class="ssc_main_linkedin"><span class="ssc_ico_linkedin"><img src="' + options.linkedin_ico + '"/></span><span class="ssc_count_linkedin">' + (data.count || 0) + '</span></span>'); //wyswietlamy gdy linkedin
+                    });
                 } //koniec instrukcji warunkowej
 
             }); //koniec iteracji
