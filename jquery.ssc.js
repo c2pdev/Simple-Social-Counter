79fe17df7b4b610b22a87f8762db8de2d1fd7a21
Dawid Myszczyszyn <dawidmyszczyszyn@gmail.com>
2013-07-27 03:37:31 +0100

    naprawa kolejności wyświetlania oraz usunięcie Delicious :)

--- jquery.ssc.js
+++ jquery.ssc.js
@@ -7,7 +7,7 @@
             twitter_ico: 'images/twitter_ssc.png',
             linkedin_ico: 'images/linkedin_ssc.png',
             facebook_ico: 'images/facebook_ssc.png',
-            url: $(location).attr('href')
+            url: window.location.href
         }, options);
 
         return this.each(function ()
@@ -17,25 +17,30 @@
             var social = options.social.split(","); //rozbicie na elementy
             $.each(social, function (i)//poczatek iteracji po tablicy
             {
+                $(target).append('<span class="ssc_main_' + social[i] + '"><span class="ssc_ico_' + social[i] + '"></span><span class="ssc_count_' + social[i] + '">0</span></span>');
+
                 if (social[i] == 'twitter') //poczatek instrukcji warunkowej if/else
                 {
+                    $(target).find('.ssc_ico_twitter').append('<img src="' + options.twitter_ico + '"/>');
                     $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=' + options.url + '&callback=?', function (data)
                     {
-                        $(target).append('<span class="ssc_main_twitter"><span class="ssc_ico_twitter"><img src="' + options.twitter_ico + '"/></span><span class="ssc_count_twitter">' + (data.count || 0) + '</span></span>'); //wyswietlamy gdy twitter
+                        $(target).find('.ssc_count_twitter').text((data.count || 0)); //wyswietlamy gdy twitter
                     });
                 } else if (social[i] == 'facebook')
                 {
+                    $(target).find('.ssc_ico_facebook').append('<img src="' + options.facebook_ico + '"/>');
                     $.getJSON('http://graph.facebook.com/' + options.url + '?callback=?', function (data)
                     {
-                        $(target).append('<span class="ssc_main_facebook"><span class="ssc_ico_facebook"><img src="' + options.facebook_ico + '"/></span><span class="ssc_count_facebook">' + (data.shares || 0) + '</span></span>'); //wyswietlamy gdy facebook
+                        $(target).find('.ssc_count_facebook').text((data.shares || 0));  //wyswietlamy gdy facebook
                     });
                 } else if (social[i] == 'linkedin')
                 {
+                    $(target).find('.ssc_ico_linkedin').append('<img src="' + options.linkedin_ico + '"/>');
                     $.getJSON('http://www.linkedin.com/countserv/count/share?url=' + options.url + '&lang=en_US&callback=?', function (data)
                     {
-                        $(target).append('<span class="ssc_main_linkedin"><span class="ssc_ico_linkedin"><img src="' + options.linkedin_ico + '"/></span><span class="ssc_count_linkedin">' + (data.count || 0) + '</span></span>'); //wyswietlamy gdy linkedin
+                        $(target).find('.ssc_count_linkedin').text((data.count || 0)); //wyswietlamy gdy linkedin
                     });
-                } //koniec instrukcji warunkowej
+                }//koniec instrukcji warunkowej
 
             }); //koniec iteracji
 
